3f8a3fdff0f2753e264deb149bbc9193b0d1bb14
Matt Zhang <mcmattzhang@gmail.com>
2021-01-06 15:53:53 -0500

    env variable for token

--- bot.py
+++ bot.py
@@ -1,13 +1,22 @@
 import os
+from dotenv import load_dotenv
+
 import discord
 from discord.ext import commands
 
-token="Nzk2NDczODU5Mjc2NDA2ODA0.X_YcFQ.CtaZt53bG4EzEGfAIctOckQTCYs"
+from bs4 import BeautifulSoup
+import requests
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
 client = commands.Bot(command_prefix = ".", help_command=None)
 
 @client.command()
 async def beep(ctx):
     await ctx.send(f'boop {client.latency * 1000}')
 
+@client.event
+async def on_ready():
+    print('aye aye captain')
 
 client.run(token)
